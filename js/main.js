c481cfd891fdf132f445eba9f1198f3823ddbc87
GlitchCat16 <45367753+GlitchCat16@users.noreply.github.com>
2019-11-16 15:21:24 -0500

    new main

--- js/main.js
+++ js/main.js
@@ -229,6 +229,21 @@ function initialize() {
   //
 }
 
+window.addEventListener('touchstart', function () {
+
+  // create empty buffer
+  var buffer = myContext.createBuffer(1, 1, 22050);
+  var source = myContext.createBufferSource();
+  source.buffer = buffer;
+
+  // connect to output (your speakers)
+  source.connect(myContext.destination);
+
+  // play the file
+  source.noteOn(0);
+
+}, false);
+
 function update() {
   if (arToolkitSource.ready !== false) arToolkitContext.update(arToolkitSource.domElement);
 
